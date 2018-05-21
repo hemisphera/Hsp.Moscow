dd7867bf430376227a9e76f9e324a6f23922e5ec
The Dumb <hemisphera@users.noreply.github.com>
2018-05-21 20:16:56 +0200

    Added script to build release package + updated .gitignore

---
+++ package.ps1
@@ -0,0 +1,10 @@
+New-Item -ItemType Directory "$PSScriptRoot\Release" -Force | Out-Null
+New-Item -ItemType Directory "$PSScriptRoot\Release\Plugins" -Force | Out-Null
+
+Get-ChildItem "$PSScriptRoot\Hsp.Moscow\bin\Release" | Copy-Item -Destination "$PSScriptRoot\Release"
+Get-ChildItem "$PSScriptRoot\Hsp.Moscow.Plugins\bin\Release" | Copy-Item -Destination "$PSScriptRoot\Release\Plugins"
+
+Get-ChildItem "$PSScriptRoot\Release" -Filter "*.xml" -Recurse | Remove-Item
+Get-ChildItem "$PSScriptRoot\Release" -Filter "*.pdb" -Recurse | Remove-Item
+
+Remove-Item "$PSScriptRoot\Release\Plugins\Hsp.Moscow.Extensibility.dll"
