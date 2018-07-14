8f605b54f5d66c603e6466b4cefce80b5341dfa9
Hemisphera <info@hemisphera.com>
2018-07-14 21:54:04 +0200

    WIP

---
+++ Msgr.Client.Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MessageR;
+
+namespace Msgr.Client.Test
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+
+      var cl = new MsgrClient("Schmoazi");
+      cl.Connect();
+      Thread.Sleep(500);
+      cl.Send("Main", "Hello1");
+      cl.Send("Main", "Hello2");
+      cl.Send("Main", "Hello3");
+      cl.Send("Main", "Hello4");
+      cl.Send("Main", "Hello5");
+      cl.Send("Main", "Hello6");
+      var msg = "";
+      while (msg != "exit")
+      {
+        msg = Console.ReadLine();
+        cl.Send("", msg);
+      }
+
+      Console.WriteLine("done");
+      Console.ReadLine();
+    }
+
+  }
+}
---
+++ Msgr.Server.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageR;
+
+namespace Msgr.Server.Test
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var sv = new MsgrServer();
+      //sv.ClientConnected += (s, e) => { Console.WriteLine($"Client {e.Name} connected."); };
+      sv.MessageReceived += (s, e) => Console.WriteLine($"{e.Sender} to {e.Recipient}: {e.Subject}");
+      Console.ReadLine();
+
+    }
+  }
+}
